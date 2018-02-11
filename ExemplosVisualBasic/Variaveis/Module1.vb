9a4266dae90012970f275e08785f5ba339b9d5d2
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-02-11 14:39:31 -0200

    Variaveis e tipos de operações visual basic

---
+++ ExemplosVisualBasic/Variaveis/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num1, num2 As Integer
+        Dim soma, subt, mult, div, modl, exp As Integer
+        Dim dec As Decimal
+
+        Console.WriteLine("Digite o primeiro valor :  ")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Digite o segundo valor: ")
+        num2 = Console.ReadLine()
+
+        soma = num1 + num2
+        subt = num1 - num2
+        mult = num1 * num2
+        div = num1 \ num2
+        modl = num1 Mod num2
+        exp = num1 ^ num2
+        dec = num1 / num2
+
+        Console.WriteLine("O primeiro número + o segundo número é {0}", soma)
+        Console.WriteLine("O primeiro número - o segundo número é {0}", subt)
+        Console.WriteLine("O primeiro número * o segundo número é {0}", mult)
+        Console.WriteLine("O primeiro número \ o segundo número é {0}", div)
+        Console.WriteLine("O primeiro número MOD o segundo número é {0}", modl)
+        Console.WriteLine("O primeiro número ^ o segundo número é {0}", exp)
+        Console.WriteLine("O primeiro número / o segundo número é {0}", dec)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
