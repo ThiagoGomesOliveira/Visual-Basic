71745a86a19ebd33f7f9199eb4c39f6df9caf69b
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-02-11 15:23:05 -0200

    Estrutura de repetição While Visual Basic

---
+++ ExemplosVisualBasic/EstruturasRepeticoes/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num As Integer
+
+        While num <= 100
+            Console.WriteLine(num)
+            num = num + 2
+        End While
+        Console.ReadKey()
+    End Sub
+
+End Module
