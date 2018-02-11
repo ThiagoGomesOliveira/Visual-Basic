2f1a5df5a4796e5a586b000d8d3c4464b1e45700
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-02-11 14:57:48 -0200

    If Else Visual Basic

---
+++ ExemplosVisualBasic/CondicoesCompostas/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim pontos As Integer
+        Console.WriteLine("Entre com a pontuação ")
+
+        pontos = Console.ReadLine()
+
+        If pontos >= 9 Then
+            Console.WriteLine("Você foi aprovado com  A")
+            Console.ReadKey()
+        ElseIf pontos >= 7 Then
+            Console.WriteLine("Você foi aprovado com  B")
+            Console.ReadKey()
+        ElseIf pontos >= 5 Then
+            Console.WriteLine("Você foi aprovado com  C")
+            Console.ReadKey()
+        ElseIf pontos >= 3 Then
+            Console.WriteLine("Você foi aprovado com  D")
+            Console.ReadKey()
+        ElseIf pontos < 3 Then
+            Console.WriteLine("Você foi Reprovado")
+            Console.ReadKey()
+        End If
+
+
+    End Sub
+
+End Module
---
+++ ExemplosVisualBasic/CondicoesCompostas/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("CondicoesCompostas")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CondicoesCompostas")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("584f0f11-908c-4977-9733-45a71ecf0d2c")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' utilizando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
