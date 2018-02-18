373ada8589ac26a9e168195ee16bb8b4888107ce
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-02-18 08:39:42 -0300

    Exemplo de Função  Visual Basic

---
+++ ExemplosVisualBasic/Funcoes/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num As Integer
+        Dim resultado As Integer
+
+
+        Console.WriteLine("Entre com um número")
+        num = Console.ReadLine()
+        resultado = Calc(num)
+        Console.WriteLine()
+        Console.WriteLine("Soma do número com ele mesmo é {0}", resultado)
+        Console.WriteLine()
+        Console.WriteLine("número Informado {0}", num)
+        Console.ReadKey()
+
+    End Sub
+
+    Function Calc(ByVal num As Integer) As Integer
+        'Funcao que soma o número que é passado por parametro por ele mesmo e retorna a soma
+        num = num + num
+        Return num
+    End Function
+
+End Module
---
+++ ExemplosVisualBasic/Funcoes/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Funcoes")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Funcoes")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("0d2c2fa4-9082-47e6-919c-25f77b7af0c5")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' utilizando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
